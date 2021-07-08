8d5e24cd25b75851fe1620c1ae495acda9996fc6
Guto Xavier <gutoffline@gmail.com>
2021-07-07 21:26:49 -0300

    exemplo de combobox

---
+++ exemplo/exemplo/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace exemplo
+{
+    partial class frmExemplo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
+            this.cmbModelo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbMarca
+            // 
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Items.AddRange(new object[] {
+            "teste",
+            "teste",
+            "teste"});
+            this.cmbMarca.Location = new System.Drawing.Point(88, 235);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(121, 21);
+            this.cmbMarca.TabIndex = 0;
+            this.cmbMarca.SelectedIndexChanged += new System.EventHandler(this.cmbMarca_SelectedIndexChanged);
+            this.cmbMarca.SelectedValueChanged += new System.EventHandler(this.cmbMarca_SelectedValueChanged);
+            this.cmbMarca.MouseClick += new System.Windows.Forms.MouseEventHandler(this.cmbMarca_MouseClick);
+            // 
+            // cmbModelo
+            // 
+            this.cmbModelo.FormattingEnabled = true;
+            this.cmbModelo.Items.AddRange(new object[] {
+            "teste",
+            "teste",
+            "teste"});
+            this.cmbModelo.Location = new System.Drawing.Point(280, 235);
+            this.cmbModelo.Name = "cmbModelo";
+            this.cmbModelo.Size = new System.Drawing.Size(121, 21);
+            this.cmbModelo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Marca:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Modelo:";
+            // 
+            // frmExemplo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbModelo);
+            this.Controls.Add(this.cmbMarca);
+            this.Name = "frmExemplo";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmExemplo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbMarca;
+        private System.Windows.Forms.ComboBox cmbModelo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ exemplo/exemplo/Form1.cs
@@ -0,0 +1,84 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exemplo
+{
+    public partial class frmExemplo : Form
+    {
+        public frmExemplo()
+        {
+            InitializeComponent();
+        }
+
+        private void frmExemplo_Load(object sender, EventArgs e)
+        {
+            string sqlBuscar = "SELECT * FROM marca_carros";
+            string bancoDeDados = "server=localhost;user id=root;password=;database=locadora;";
+
+            MySqlConnection conexao = new MySqlConnection(bancoDeDados);
+            try
+            {
+                conexao.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(sqlBuscar, conexao);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                cmbMarca.DataSource = dt;
+                cmbMarca.ValueMember = "id_marca";
+                cmbMarca.DisplayMember = "nome";
+                cmbMarca.Refresh();
+
+                conexao.Close();
+            }
+            catch (MySqlException erro)
+            {
+                
+            }
+        }
+
+        private void cmbMarca_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sqlBuscar = $"SELECT id_auto, modelo , fk_marca_carros  FROM automoveis where fk_marca_carros = {cmbMarca.SelectedValue.ToString()}";
+
+            string bancoDeDados = "server=localhost;user id=root;password=;database=locadora;";
+
+            MySqlConnection conexao = new MySqlConnection(bancoDeDados);
+            try
+            {
+                conexao.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(sqlBuscar, conexao);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                cmbModelo.DataSource = dt;
+                cmbModelo.ValueMember = "id_auto";
+                cmbModelo.DisplayMember = "modelo";
+                cmbModelo.Refresh();
+
+                conexao.Close();
+            }
+            catch (MySqlException erro)
+            {
+                
+            }
+        }
+
+        private void cmbMarca_SelectedValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cmbMarca_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+    }
+}
